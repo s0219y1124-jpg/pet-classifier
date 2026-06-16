acf078262a301cb35f00255ab2d827ed265ddbd0
s0219y1124-jpg <s0219y1124@gmail.com>
2026-06-16 23:43:44 +0900

    Fix CORS for Vercel

--- backend/api.py
+++ backend/api.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://yourdomain.com"],
+    "https://pet-classifier-we1u.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
